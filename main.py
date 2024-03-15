44ec82d4f21fc4959079927932e8261b2046ee3b
Priyanga2020 <sairiya1833@gmail.com>
2024-03-15 19:30:45 +0530

    Modified

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+df = pd.read_csv("Prodigy-intern/API_SP.POP.TOTL_DS2_en_csv_v2_85")
+print(df.head)
